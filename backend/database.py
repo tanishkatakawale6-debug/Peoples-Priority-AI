d1e6dfbd48b7ab9568aa192d355e17bee3ef9123
tanishkatakawale6 <tanishkatakawale6@gmail.com>
2026-07-22 18:37:26 +0530

    Add mysql port

--- backend/database.py
+++ backend/database.py
@@ -7,7 +7,7 @@ load_dotenv()
 def get_db_connection():
     connection = mysql.connector.connect(
         host=os.getenv("DB_HOST"),
-        port=int(os.getenv(DB_PORT)),
+        port=int(os.getenv("DB_PORT")),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
